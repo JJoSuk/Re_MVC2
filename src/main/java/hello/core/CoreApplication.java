9a8f63957e92e6c876332026bee04da082f291c4
JJoSuk <loveyou55042@gmail.com>
2023-07-03 23:51:09 +0900

    MVC2

---
+++ src/main/java/hello/core/CoreApplication.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
